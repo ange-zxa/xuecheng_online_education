953556aa5fbcf7db1b0c8b80bfcd3e4d0688b198
zhuxianao <zhuxianao2001@163.com>
2023-07-27 16:11:53 +0800

    完成查询整个接口并测试通过

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,8 +4,10 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,14 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
  * \* Description:查询接口
  * \
  */
-@Api(value ="课程内容管理接口",tags = "课程内容管理接口")
+@Api(value = "课程内容管理接口", tags = "课程内容管理接口")
 @RestController
 public class CourseBaseInfoController {
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
 
     @ApiOperation("课程查询接口")
     @PostMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
-        return null;
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto) {
+        PageResult<CourseBase> pageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParamsDto);
+        return pageResult;
     }
 
 }
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -0,0 +1,19 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: 祝先澳
+ * \* Date: 2023/7/27
+ * \* Time: 15:15
+ * \* Description:课程信息管理接口
+ * \
+ */
+public interface CourseBaseInfoService {
+    //课程分类查询
+    PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+}
